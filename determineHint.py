fb030b143ed670cb04e1ebcffd14e54714e4757f
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2023-01-03 19:30:02 +0000

    working

--- determineHint.py
+++ determineHint.py
@@ -1,18 +1,17 @@
 import random
 
-def determineHint(guess, code):
 
+def determineHint(guess, code):
 
   newGuess = list(guess)
   newCode = list(code)
-  
+
   defaultList = []
 
   # Count the number of correct colors in the correct position
   correct_color_and_position = 0
   correct_color_wrong_position = 0
 
-  
   for i in range(len(newGuess)):
     if newGuess[i] == newCode[i]:
       correct_color_and_position += 1
@@ -20,14 +19,13 @@ def determineHint(guess, code):
       newGuess[i] = ""
       newCode[i] = ""
 
-      
   # Count the number of correct colors in the wrong position
-  
+
   for i in range(len(newGuess)):
     if newGuess[i] != "":
       if newGuess[i] in code:
         correct_color_wrong_position += 1
-        newCode[newCode.index(newGuess[i])] = ""
+        #newCode[newCode.index(newGuess[i])] = ""
 
   for i in range(correct_color_and_position):
     defaultList.append("X")
@@ -36,7 +34,6 @@ def determineHint(guess, code):
 
   while len(defaultList) < 4:
     defaultList.append("-")
-
 
   #print("original " + str(defaultList))
   random.shuffle(defaultList)
